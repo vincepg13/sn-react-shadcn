1c458a2bce101ee2d136ca7dc0f3ceaca4abe18a
vincepg13 <se20basketballcamp@yahoo.com>
2025-09-21 10:16:58 +0100

    strict esm, removed cjs

---
+++ src/exports/standalone.index.ts
@@ -0,0 +1,8 @@
+//Standalone Form Elements
+export { SnTabs } from '../components/sn-ui/sn-tabs'
+export { SnRecordPicker } from '../components/sn-form/sn-record-picker'
+export { SnClippy } from '../components/sn-ui/sn-attachments/sn-clippy'
+export { SnActivity } from '../components/sn-ui/sn-activity/sn-activity'
+
+//Types
+export type { SnRecordPickerItem, SnRecordPickerList } from '../types/form-schema'
